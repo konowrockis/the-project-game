eb3e67bdd5b59ef582f2a3e90a629707c9a3999d
Sebastian Konowrocki <sebastian@appetiq.com>
2017-03-18 19:18:41 +0100

    Implement options parsing using IoC

---
+++ src/TheProjectGame.Settings/Options/NetworkOptions.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace TheProjectGame.Settings.Options
+{
+    public class NetworkOptions
+    {
+        [Option('p', "port")]
+        public int Port { get; set; }
+
+        [Option('a', "address")]
+        public string Address { get; set; }
+
+        [Option(nameof(NetworkOptions) + "." + nameof(KeepAliveInterval))]
+        public int KeepAliveInterval { get; set; }
+    }
+}
---
+++ src/TheProjectGame.Settings/SettingsModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Autofac;
+
+namespace TheProjectGame.Settings
+{
+    public class SettingsModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<OptionsParser>()
+                .WithParameter("args", Environment.GetCommandLineArgs())
+                .AsSelf().SingleInstance();
+        }
+    }
+}
