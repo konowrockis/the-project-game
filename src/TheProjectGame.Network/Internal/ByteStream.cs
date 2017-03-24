7f11261c47904eefae0c4d11d47370fb36d12c7f
Sebastian Konowrocki <sebastian@appetiq.com>
2017-03-24 23:08:23 +0100

    Move ByteStream to separate file

---
+++ src/TheProjectGame.Network/Internal/ByteStream.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Sockets;
+
+namespace TheProjectGame.Network.Internal
+{
+    public class ByteStream : NetworkStream
+    {
+        private const byte ETB = 0x17;
+
+        public ByteStream(Socket s) : base(s)
+        { }
+
+        public override int Read(byte[] buffer, int offset, int size)
+        {
+            int read = base.Read(buffer, offset, size);
+
+            if (read == 1 && buffer[0] == ETB)
+            {
+                return Read(buffer, offset, size);
+            }
+
+            return read;
+        }
+
+        public override int ReadByte()
+        {
+            int b = base.ReadByte();
+            return b == ETB ? ReadByte() : b;
+        }
+    }
+}
--- src/TheProjectGame.Network/Internal/Client/ClientHandler.cs
+++ src/TheProjectGame.Network/Internal/Client/ClientHandler.cs
@@ -13,46 +13,6 @@ using TheProjectGame.Settings.Options;
 
 namespace TheProjectGame.Network
 {
-    public class ByteStream : NetworkStream
-    {
-        private const byte ETB = 0x17;
-
-        public ByteStream(Socket s) : base(s)
-        { }
-
-        public override int Read(byte[] buffer, int offset, int size)
-        {
-            int read = base.Read(buffer, offset, size);
-
-            if (read == 1 && buffer[0] == ETB)
-            {
-                return Read(buffer, offset, size);
-            }
-
-            int move = 0;
-
-            for (int i = 0; i < read; i++)
-            {
-                if (buffer[i] == ETB)
-                {
-                    move++;
-                }
-                else if (move != 0)
-                {
-                    buffer[i - move] = buffer[i];
-                }
-            }
-
-            return read - move;
-        }
-
-        public override int ReadByte()
-        {
-            int b = base.ReadByte();
-            return b == ETB ? ReadByte() : b;
-        }
-    }
-
     internal class ClientHandler : INetworkHandler
     {
         private readonly IClientSocket socket;
