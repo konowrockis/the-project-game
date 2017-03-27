45684ef9fa0b12bce81e84253cf720385366772d
Sebastian Konowrocki <sebastian@appetiq.com>
2017-03-27 23:38:16 +0200

    Add Communication Server integration tests

---
+++ test/TheProjectGame.IntegrationTests/CommunicationServerTests.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheProjectGame.IntegrationTests
+{
+    [TestClass]
+    public class CommunicationServerTests
+    {
+        private Process communicationServerProcess;
+
+        private const int port = 20000;
+        private const string communicationServerAddress = "127.0.0.1";
+        private const string communicationServerLocation = @"..\..\..\..\src\TheProjectGame.CommunicationServer\bin\Debug";
+        private const string communicationServerName = "TheProjectGame.CommunicationServer.exe";
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            communicationServerProcess = new Process();
+
+            communicationServerProcess.StartInfo.FileName = communicationServerName;
+            communicationServerProcess.StartInfo.Arguments = $"--port {port}";
+            communicationServerProcess.StartInfo.WorkingDirectory = communicationServerLocation;
+            communicationServerProcess.Start();
+        }
+
+        [TestMethod]
+        public void Connect_to_CommunicationServer()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(communicationServerAddress), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect(endPoint);
+
+            Assert.IsTrue(socket.Connected);
+        }
+
+        [TestMethod]
+        public void Get_response_for_GetGames_message()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(communicationServerAddress), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            byte[] buffer = new byte[10240];
+
+            socket.Connect(endPoint);
+            socket.Send(GetGetGamesMessage());
+            socket.Receive(buffer);
+
+            Assert.IsTrue(socket.Connected);
+            Assert.IsTrue(Encoding.UTF8.GetString(buffer).Contains("RegisteredGames"));
+        }
+
+        [TestMethod]
+        public void Reconnect_with_CommunicationServer()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(communicationServerAddress), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket reconnectSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect(endPoint);
+            socket.Dispose();
+            reconnectSocket.Connect(endPoint);
+
+            Assert.IsTrue(reconnectSocket.Connected);
+        }
+
+        [TestMethod]
+        public void Stress_test_CommunicationServer()
+        {
+            const int numberOfClients = 100;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(communicationServerAddress), port);
+            List<Socket> sockets = Enumerable.Range(0, numberOfClients)
+                .Select(i => new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)).ToList();
+
+            sockets.ForEach(socket => socket.Connect(endPoint));
+
+            sockets.TrueForAll(socket => socket.Connected);
+        }
+
+        private byte[] GetGetGamesMessage()
+        {
+            return Encoding.UTF8.GetBytes("<?xml version=\"1.0\" encoding=\"utf-8\" ?><GetGames xmlns=\"http://theprojectgame.mini.pw.edu.pl/\" />");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            communicationServerProcess.Kill();
+        }
+    }
+}
---
+++ test/TheProjectGame.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TheProjectGame.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TheProjectGame.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3404d6f4-0910-4865-b1d3-3aed111cf3ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
