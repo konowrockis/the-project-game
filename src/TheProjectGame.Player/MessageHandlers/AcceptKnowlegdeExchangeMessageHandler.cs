55c84ee154f708f6817a0e7085ea51df81047113
Staszek Wasilkowski <wasilkowskis@student.mini.pw.edu.pl>
2017-04-27 21:02:45 +0200

    Add accept knowledge exchange message handler

---
+++ src/TheProjectGame.Player/MessageHandlers/AcceptKnowlegdeExchangeMessageHandler.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+using TheProjectGame.Contracts.Messages.CommunicationActions;
+using TheProjectGame.Contracts.Messages.PlayerActions;
+using TheProjectGame.Contracts.Messages.Structures;
+using TheProjectGame.Game;
+using TheProjectGame.Messaging;
+using TheProjectGame.Player.Game;
+
+namespace TheProjectGame.Player.MessageHandlers
+{
+    class AcceptKnowlegdeExchangeMessageHandler : MessageHandler<AcceptExchangeRequestMessage>
+    {
+        private readonly IMessageWriter messageWriter;
+        private readonly IPlayerLogic playerLogic;
+        private readonly IPlayerKnowledge playerKnowledge;
+
+        public AcceptKnowlegdeExchangeMessageHandler(
+            IMessageWriter messageWriter,
+            IPlayerKnowledge playerKnowledge,
+            IPlayerLogic playerLogic)
+        {
+            this.messageWriter = messageWriter;
+            this.playerLogic = playerLogic;
+            this.playerKnowledge = playerKnowledge;
+        }
+        public override void Handle(AcceptExchangeRequestMessage message)
+        {
+            SendData(message);
+        }
+
+        private void SendData(AcceptExchangeRequestMessage message)
+        {
+            var taskTiles = playerKnowledge.GameState.Board.Fields.OfType<TaskTile>().ToList()
+                .Select(t => new TaskField()
+                {
+                    DistanceToPiece = t.DistanceToPiece,
+                    PieceId = t.Piece?.Id ?? 0,
+                    PieceIdSpecified = t.Piece != null,
+                    Timestamp = t.Timestamp,
+                    PlayerId = t.Player?.Id ?? 0,
+                    PlayerIdSpecified = t.Player != null,
+                    X = t.X,
+                    Y = t.Y
+                })
+                .ToList();
+
+            var goalTiles = playerKnowledge.GameState.Board.Fields.OfType<GoalTile>().ToList()
+                .Select(t => new GoalField()
+                {
+                    Type = t.Type,
+                    Team = t.Team,
+                    Timestamp = t.Timestamp,
+                    PlayerId = t.Player?.Id ?? 0,
+                    PlayerIdSpecified = t.Player != null,
+                    X = t.X,
+                    Y = t.Y
+                })
+                .ToList();
+
+            var pieces = playerKnowledge.GameState.Board.Pieces
+                .Select(p => new Piece()
+                {
+                    Id = p.Id,
+                    Type = p.Type,
+                    Timestamp = p.Timestamp,
+                    PlayerId = p.Player?.Id ?? 0,
+                    PlayerIdSpecified = p.Player != null
+                })
+                .ToList();
+
+            var data = new DataMessage()
+            {
+                TaskFields = taskTiles,
+                GoalFields = goalTiles,
+                Pieces = pieces,
+                PlayerId = message.SenderPlayerId
+            };
+
+            messageWriter.Write(data);
+        }
+    }
+}
