c076b5f6e4e5d695767b0ebef7ec3d7c05270375
Staszek Wasilkowski <wasilkowskis@student.mini.pw.edu.pl>
2017-04-27 22:53:58 +0200

    Modified test to accept exchange knowledge message handler

--- test/TheProjectGame.Player.Tests/AcceptKnowledgeExchangeMessageHandlerTests.cs
+++ test/TheProjectGame.Player.Tests/AcceptKnowledgeExchangeMessageHandlerTests.cs
@@ -5,6 +5,8 @@ using TheProjectGame.Contracts.Messages.PlayerActions;
 using TheProjectGame.Messaging;
 using TheProjectGame.Player.Game;
 using TheProjectGame.Player.MessageHandlers;
+using TheProjectGame.Game;
+using System.Collections.Generic;
 
 namespace TheProjectGame.Player.Tests
 {
@@ -15,10 +17,20 @@ namespace TheProjectGame.Player.Tests
         public void Send_DataMessage_after_receiving_AcceptExchangeRequestMessage()
         {
             IMessageWriter writer = Substitute.For<IMessageWriter>();
-            IPlayerKnowledge knowledge = Substitute.For<IPlayerKnowledge>();
+            var gameState = new GameState(1);
+            var knowledge = new PlayerKnowledge();
+            var player = new GamePlayer(1);
+            var board = new Board(10, 2, 2, .5);
             var handler = new AcceptKnowlegdeExchangeMessageHandler(writer, knowledge);
             var message = new AcceptExchangeRequestMessage();
+            var playersList = new List<GamePlayer>(1);
 
+           
+            playersList.Add(player);
+            board.Init(playersList, 5, 5);
+            board.PlaceNewPiece();
+            gameState.Board = board;
+            knowledge.Init(player, "test", gameState);
             handler.Handle(message);
 
             writer.Received().Write(Arg.Any<DataMessage>());
